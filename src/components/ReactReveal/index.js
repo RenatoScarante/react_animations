3dfaab04dc81b474a946c1be447903cc04a36a32
Renato Scarante <renato.scarante@ltm.digital>
2020-01-16 11:32:56 -0300

    Updates

--- src/components/ReactReveal/index.js
+++ src/components/ReactReveal/index.js
@@ -8,11 +8,12 @@ const animatedList = [1, 2, 3]
 const ReactReveal = () => {
   return (
     <section className="vw-100 vh-100">
+      <a href="http://www.react-reveal.com" target="_blank">http://www.react-reveal.com</a>
       {
         animatedList.map((item, key) => (
           <Row className="bg-light vh-100 align-middle border" key={key}>
             <Col className="text-center align-self-center">
-              <Fade top>
+              <Fade down>
                 <h1>Item {item}</h1>
               </Fade>
             </Col>
